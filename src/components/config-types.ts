81d9085d436411a883ec7e74dd7a9c9f5e31a2c4
knightcoder <rohitspujari@gmail.com>
2025-09-17 16:33:02 -0500

    added mode in the theme

--- src/components/config-types.ts
+++ src/components/config-types.ts
@@ -18,9 +18,15 @@ export type Data = Record<string, string | number | boolean>[];
 
 export type DataArray = Data[];
 
+export type CustomCardTheme = {
+  colors?: string[];
+  mode?: 'light' | 'dark' | 'system';
+};
+
 export type SingleInputVisualProps = {
   data: Data;
   settings?: Record<string, string | number | boolean>;
+  theme?: CustomCardTheme;
 };
 
 export type MultiInputVisualProps = {
